77451be05b760678cae26158d3b018fb6ca85e24
Song Yang <songyang@thoughtworks.com>
2015-03-30 20:34:03 +0800

    data | extract data template

--- js/stores/data-services.js
+++ js/stores/data-services.js
@@ -4,35 +4,9 @@ var moment = require('moment');
 var api = require('./../libs/douban-api');
 
 var WormAction = require('../actions/WormActions');
+var _regionsData = require('./data.tpl');
+
 var inited = false;
-
-var goal = {
-  title: '目标',
-  list: [{
-    preText: '年度目标',
-    unit: null,
-    sufText: '本'
-  }, {
-    preText: '已完成',
-    unit: 11,
-    sufText: '本'
-  }]
-};
-
-var plan = {
-  title: '计划',
-  list: [{
-    preText: '每',
-    unit: null,
-    sufText: '天，就得完成一本'
-  }, {
-    preText: '当前速度',
-    unit: null,
-    sufText: '天/本'
-  }]
-};
-
-var _regionsData = [].concat(goal, plan);
 
 function getDaysDiff () {
   var thisYear = moment().year();
---
+++ js/stores/data.tpl.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var regionsData = [
+  {
+    title: '目标',
+    list: [{
+      preText: '年度目标',
+      unit: null,
+      sufText: '本'
+    }, {
+      preText: '已完成',
+      unit: 11,
+      sufText: '本'
+    }]
+  }, {
+    title: '计划',
+    list: [{
+      preText: '每',
+      unit: null,
+      sufText: '天，就得完成一本'
+    }, {
+      preText: '当前速度',
+      unit: null,
+      sufText: '天/本'
+    }]
+  }
+
+];
+
+module.exports = regionsData;
